6d0e3f9c0037e9114373ff1584b3e8ef3665bb46
LukasHedegaard <lh@eng.au.dk>
2021-03-30 14:09:57 +0200

    Bump library version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name="office31",
-    version="0.1.1",
+    version="0.1.3",
     author="Lukas Hedegaard",
     description="Revised splits for Office31 domain adaptation experiments",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LukasHedegaard/datasetops",
+    url="https://github.com/LukasHedegaard/office31",
     python_requires=">=3.6",
     packages=find_packages("src"),
     package_dir={"": "src"},
